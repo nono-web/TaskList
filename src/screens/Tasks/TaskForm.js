27ef23c68650d77bd18fc37f8f025d1ed19bb62a
[REM-FR]Arnaud Didier <84334523+nono-web@users.noreply.github.com>
2023-01-20 17:41:59 +0100

    First commit

---
+++ src/screens/Tasks/TaskForm.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { Text, StyleSheet, View, TextInput, Button } from 'react-native';
+
+const TaskForm = ({onAddTask}) => {
+  const [newTitle, setNewTitle] = useState(null);
+  const onChangeText = (val) => {
+    setNewTitle(val);
+  };
+
+  const onAddNewTask = () => {
+    if(newTitle === null) return
+
+    onAddTask(newTitle)
+    setNewTitle(null)
+  };
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={newTitle}
+        placeholder="Nouvelle tâche"
+        
+      />
+      <Button title="Ajouter" onPress={onAddNewTask} color="blue" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingLeft : 20,
+    paddingHorizontal : 20,
+    marginTop : 10,
+  },
+  input: {
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius : 5,
+    width : "70%",
+    height : 40,
+  },
+  
+});
+
+export default TaskForm;
---
+++ src/screens/Tasks/index.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import Header from '../../components/Header';
+import { StyleSheet, View, FlatList, Pressable, Text } from 'react-native';
+import TaskTile from './TaskTile';
+import TaskForm from './TaskForm';
+import FloatingBtn from '../../components/FloatingBtn';
+import Counter from '../../components/Counter';
+
+
+const TaskScreen = () => {
+  const [tasks, setTasks] = useState([]);
+  const [isFormVisible, setIsFormVisible] = useState(false);
+  console.log(tasks)
+
+  const renderItem = ({ item }) => {
+    return (
+      <TaskTile task={item} onUpdateTask={onUpdateTask} onDelete={onDelete} />
+    );
+  };
+
+  const onAddTask = (title) => {
+    setTasks([
+      ...tasks,
+      {
+        id: Date.now(),
+        title,
+        isCompleted: false,
+      },
+    ]);
+  };
+
+  const onDelete = (id) => {
+    let newTask = [];
+
+    tasks.forEach((t) => {
+      if (t.id !== id) {
+        newTask.push(t);
+        return;
+      }
+    });
+    setTasks(newTask);
+  };
+
+  const onUpdateTask = (id) => {
+    let newTask = [];
+
+    tasks.forEach((t) => {
+      if (t.id !== id) {
+        newTask.push(t);
+        return;
+      }
+      newTask.push({
+        id,
+        title: t.title,
+        isCompleted: !t.isCompleted,
+      });
+    });
+    setTasks(newTask);
+  };
+
+  const onPressBtn = () => {
+    setIsFormVisible(!isFormVisible);
+  };
+  return (
+    <>
+      <FlatList
+        ListHeaderComponent={
+          <>
+            <Header />
+            {isFormVisible && <TaskForm onAddTask={onAddTask} />}
+            <View style={styles.containerCounter}>
+            <Counter nb={tasks.length}  title="Tâches créées" />
+            <Counter nb={tasks.filter(t=>t.isCompleted === true).length}  title="Tâches effectuées" />
+            </View>
+          </>
+        }
+        contentContainerStyle={{ flexGrow: 1 }}
+        data={tasks}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={renderItem}
+      />
+     <FloatingBtn toggle={onPressBtn} isOpen={isFormVisible}/>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  containerCounter: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  marginTop: 10,
+  paddingHorizontal:20,
+  }
+});
+
+export default TaskScreen;
